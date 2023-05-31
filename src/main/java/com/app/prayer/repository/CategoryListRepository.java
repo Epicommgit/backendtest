59efe13605bb9d5a5a2558ba7a45c477806de0ba
abcde <abcde@gmail.com>
2023-05-31 12:04:07 +0530

    demo code added

--- src/main/java/com/app/prayer/repository/CategoryListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.prayer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.prayer.entity.Category;
-
-public interface CategoryListRepository extends JpaRepository<Category, Integer>{
-
-}
